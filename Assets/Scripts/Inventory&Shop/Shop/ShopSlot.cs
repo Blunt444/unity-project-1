5fc4fa8a3f689bb0e18da3f87c471d5cae92af8d
blunt <bluntoriginal444@gmail.com>
2026-06-26 22:16:44 +0530

    Added slot prefab, Made a info panel(Work in progress), Shopmanager who fills the shop automatically with items.

---
+++ Assets/Scripts/Inventory&Shop/Shop/ShopSlot.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopSlot : MonoBehaviour
+{
+    public ItemSO itemSO;
+    public TMP_Text itemNameText;
+    public TMP_Text priceText;
+    public Image itemImage;
+
+    private int price;
+
+
+    public void Initialize(ItemSO newItemSO,int price)
+    {
+        Debug.Log(newItemSO);
+        itemSO = newItemSO;
+        itemImage.sprite = itemSO.icon;
+        itemNameText.text = itemSO.itemName;
+        this.price = price;
+        priceText.text = price.ToString();
+    }
+}
